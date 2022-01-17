54e3a10929075cf210f68e86b4a2764d59efd9e3
Lucas O <58038695+Lucas-Olivieri@users.noreply.github.com>
2022-01-17 16:14:38 +0100

    The 2nd page of the form now displays the informations towards the offers proposed by the site

--- src/beans/ResultBean.java
+++ src/beans/ResultBean.java
@@ -1,126 +1,44 @@
 package beans;
 
+import java.io.Serializable;
 import java.util.List;
-
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
-import business.engine.Engine;
-import business.engine.SearchEntry;
+import business.engine.MockOfferBuilder;
+import business.model.Offer;
 
 @ManagedBean
-@RequestScoped
-public class ResultBean {
-	@ManagedProperty(value = "#{entryBean}")
-	private EntryBean entryBean;
+@SessionScoped
+public class ResultBean implements Serializable {
+
+	private List<Offer> offers;
 	
-	public ResultBean() {
+    private MockOfferBuilder builder = new MockOfferBuilder();
+    
+    private Offer offer;
+    
+    @PostConstruct
+    public void init() {
+    	builder.init();
+        offers = builder.getOffers();
+    }
 
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
-	@PostConstruct
-	private void printResults() {
-		setBudgetMin(entryBean.getBudgetMin());
-		setBudgetMax(entryBean.getBudgetMax());
-		setDaysOfStay(entryBean.getDaysOfStay());
-		setDifficultyPreference(entryBean.getDifficultyPreference());
-		setLocationPreference(entryBean.getLocationPreference());
+	public void setBuilder(MockOfferBuilder builder) {
+		this.builder = builder;
 	}
 
-	public EntryBean getEntryBean() {
-		return entryBean;
+	public Offer getOffer() {
+		return offer;
 	}
 
-	public void setEntryBean(EntryBean entryBean) {
-		this.entryBean = entryBean;
-	}	
-	
-	public int getBudgetMin() {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		return entry.getBudgetMin();
-	}
-
-	public void setBudgetMin(int budgetMin) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.setBudgetMin(budgetMin);
-	}
-
-	public int getBudgetMax() {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		return entry.getBudgetMax();
-	}
-
-	public void setBudgetMax(int budgetMax) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.setBudgetMax(budgetMax);
-	}
-
-	public int getDaysOfStay() {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		return entry.getDaysOfStay();
-	}
-
-	public void setDaysOfStay(int daysOfStay) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.setDaysOfStay(daysOfStay);
-	}
-
-	public int getDifficultyPreference() {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		return entry.getDifficultyPreference() ;
-	}
-
-	public void setDifficultyPreference(int difficultyPreference) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.setDifficultyPreference(difficultyPreference);
-	}
-
-	public String getLocationPreference() {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		return entry.getLocationPreference();
-	}
-
-	public void setLocationPreference(String locationPreference) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.setLocationPreference(locationPreference);
-	}
-
-	public List<String> getSearchKeywords() {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		return entry.getSearchKeywords();
-	}
-
-	public void setSearchKeywords(List<String> searchKeywords) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.setSearchKeywords(searchKeywords);
-	}
-
-	public void addSearchKeyword(String keyword) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		if (!entry.getSearchKeywords().contains(keyword)) {
-			entry.addSearchKeyword(keyword);
-		}
-	}
-
-	public void removeSearchKeyword(String keyword) {
-		Engine form = entryBean.getForm();
-		SearchEntry entry = form.getSearchEntry();
-		entry.removeSearchKeyword(keyword);
+	public void setOffer(Offer offer) {
+		this.offer = offer;
 	}
 }
