7797e4c5c7836e56171ed8bf1e2cd4ee788c2f70
GeoffroyK <52528938+GeoffroyK@users.noreply.github.com>
2022-01-17 14:17:24 +0100

    Test not working :angry:

--- src/test/manual/TestJDBC.java
+++ src/test/manual/TestJDBC.java
@@ -3,8 +3,8 @@ package test.manual;
 import persistence.LocationImplementation;
 
 public class TestJDBC {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		LocationImplementation locAPI = new LocationImplementation();
 		locAPI.getHotelByPrice(0, 100);
-	}
+	}*/
 }
