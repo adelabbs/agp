177f58008374e00897294b20751c0f3afdfd6c3d
GeoffroyK <52528938+GeoffroyK@users.noreply.github.com>
2022-01-18 16:44:05 +0100

    :angry: GIT ERROR 404

--- src/test/manual/TestJDBC.java
+++ src/test/manual/TestJDBC.java
@@ -1,17 +1,7 @@
 package test.manual;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import business.model.location.Coordinates;
-import business.model.location.Hotel;
-import business.model.location.Site;
-import business.model.transport.Transport;
-import business.spring.SpringIoC;
 import persistence.LocationImplementation;
 import persistence.edb.ExecutionPlan1;
-import persistence.edb.operator.Operator;
-import persistence.edb.operator.Result;
 
 public class TestJDBC {
 	
